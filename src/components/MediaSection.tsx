a7ea3cd478b4647de1525ef2a4bfcb67e7336663
WuTangLan1 <finnmassari16@gmail.com>
2025-03-10 11:18:28 +0200

    Update MediaSection.tsx

--- src/components/MediaSection.tsx
+++ src/components/MediaSection.tsx
@@ -1,6 +1,7 @@
 // src/components/MediaSection.tsx
+import React from 'react'
 import { Box, Typography, styled, keyframes } from '@mui/material'
-import { useTheme } from '@emotion/react'
+import { useTheme } from '@mui/material/styles'
 
 const images = [
   { id: 1, src: '/images/personal/profile1.png', alt: 'Performance 1' },
@@ -21,31 +22,36 @@ const ScrollingWrapper = styled(Box)({
   width: 'fit-content'
 })
 
-
 export default function MediaSection({ id }: { id: string }) {
-    const theme = useTheme()
+  const theme = useTheme()
   return (
-    <Box id={id} sx={{  p: { xs: 1, md: 8 }  }}>
+    <Box id={id} sx={{ p: { xs: 1, md: 8 } }}>
       <Typography 
-                variant="h2" 
-                align="center" 
-                gutterBottom 
-                sx={{ color: theme.palette.mode === 'light' ? 'black' : 'inherit', transition: theme.transitions.create(['color'], { duration: theme.transitions.duration.standard, easing: theme.transitions.easing.easeInOut }) }}
-              >
-                Media
+        variant="h2" 
+        align="center" 
+        gutterBottom 
+        sx={{ 
+          color: theme.palette.mode === 'light' ? 'black' : 'inherit', 
+          transition: theme.transitions.create(['color'], { 
+            duration: theme.transitions.duration.standard, 
+            easing: theme.transitions.easing.easeInOut 
+          })
+        }}
+      >
+        Media
       </Typography>
       <Box sx={{ overflow: 'hidden', width: '100%', position: 'relative' }}>
-      <ScrollingWrapper>
-        {repeatedImages.map((image, index) => (
-          <Box key={`${image.id}-${index}`} sx={{ display: 'inline-block', mx: 1 }}>
-            <img
-              src={image.src}
-              alt={image.alt}
-              style={{ width: '300px', height: 'auto', display: 'block' }}
-            />
-          </Box>
-        ))}
-      </ScrollingWrapper>
+        <ScrollingWrapper>
+          {repeatedImages.map((image, index) => (
+            <Box key={`${image.id}-${index}`} sx={{ display: 'inline-block', mx: 1 }}>
+              <img
+                src={image.src}
+                alt={image.alt}
+                style={{ width: '300px', height: 'auto', display: 'block' }}
+              />
+            </Box>
+          ))}
+        </ScrollingWrapper>
       </Box>
     </Box>
   )
